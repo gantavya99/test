d23e6b51e3cba0a1ceb4123756268bd2da7c6212
Gantavya Saraswat <gantavya99@gmail.com>
2021-07-18 18:39:37 +0530

    1st question

--- geeks/crackingCodingInterview/arrayAndStrings.java
+++ geeks/crackingCodingInterview/arrayAndStrings.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class arrayAndStrings{
    public static void main(String[] args) {
-     String str="qwertyuiopasdfghjklzxcvbnm1234567890";
+     String str="qwertyuiopasdfghjklzxcaaavbnm1234567890";
      System.out.println(isUnique(str));
    }
    static boolean isUnique(String str){
@@ -29,6 +29,17 @@ public class arrayAndStrings{
         return false;
   }
    static boolean isUniqueCtCI(String str){
-       if(str.length()>128)
+       if(str.length()>128){
+           return false;
+       }
+       boolean[] charSet=new boolean[128];
+       for(int i=0;i<str.length();i++){
+           int val=str.charAt(i);
+           if(charSet[val]){
+               return false;
+           }
+           charSet[val]=true;
+       }
+       return true;
    }
 }
