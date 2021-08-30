f24c7620f23630e269e7eea6fcddafebc2107aa8
Gantavya Saraswat <gantavya99@gmail.com>
2021-08-30 09:04:53 +0530

    Stacks questions updated

---
+++ geeks/stacksques/nearestGreaterToLeft.java
@@ -0,0 +1,5 @@
+package stacksques;
+
+public class nearestGreaterToLeft {
+    static 
+}
--- geeks/stacksques/nearestGreatest.java
+++ geeks/stacksques/nearestGreatest.java
@@ -20,7 +20,7 @@ public static ArrayList<Integer> nearest(int a[]){
                 arl.add(-1);
             }
             else{
-                arl.add(s.pop());
+            s.pop();
 
             }
            }
@@ -31,12 +31,13 @@ public static ArrayList<Integer> nearest(int a[]){
   return arl;
    }
 
-public static int[] nextLargerElement(int[] arr, int n)
+public static ArrayList<Integer> nearestLargerLeft(int[] arr)
 
 { 
+    int n=arr.length;
     Stack<Integer> s = new Stack<>();              
-    int nge[] = new int[n]; 
-    for (int i = n - 1; i >= 0; i--)
+    ArrayList<Integer>arl=new ArrayList<>();
+    for (int i = 0;i<n;i++)
     {     
         if (!s.empty()) 
         {               
@@ -45,14 +46,14 @@ public static int[] nextLargerElement(int[] arr, int n)
             } 
         } 
         if(s.empty()){
-            nge[i]=-1;
+            arl.add(-1);
         }  
         else{
-            nge[i]=s.peek();
+            arl.add(s.peek());
         }
         s.push(arr[i]);                     
     } 
-    return nge;
+    return arl;
 } 
 
 public static int[] bruteForce(int a[]){
@@ -73,30 +74,34 @@ public static int[] bruteForce(int a[]){
     return temp;
 }
 
-public static int[] stacksnearlarger(int a[]){
+public static ArrayList<Integer> stacksnearlarger(int a[]){
     int n=a.length;
-    int temp[]=new int[n];
+    ArrayList<Integer>arl=new ArrayList<Integer>();
     int k=0;
     Stack<Integer>s=new Stack<Integer>();
     for(int i=n-1;i>=0;i--){
-        
+        if(s.empty()){
+            arl.add(-1);
+        }
+        else if(!s.empty()&&s.peek()>a[i]){
+            arl.add(s.peek());
+
+        }
+        else if(!s.empty()&&s.peek()<a[i]){
+            while(!s.empty()&&s.peek()<a[i]){
+                if(s.empty()){
+                    arl.add(-1);
+                }
+                else{
+                    s.pop();
+                }
+            }
         }
         s.push(a[i]);
+        }
+        return arl;
+        
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 public static int[] nearestGreaterLeft(int a[], int n){
     int res[]=new int[n];
             for(int i=1;i<n;i++){
@@ -113,8 +118,9 @@ public static int[] nearestGreaterLeft(int a[], int n){
 
 public static void main(String[] args) {
         int a[]=new int[]{1,3,2,4};
-        int res[]=bruteForce(a);
-        for(int i:res){
+        ArrayList<Integer>temp=nearestLargerLeft(a);
+        
+        for(int i:temp){
             System.out.println(i);
         }
     
